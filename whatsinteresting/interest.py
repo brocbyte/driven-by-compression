cccfaa12d5f86f5d1e52c23bbaef7978f7f65948
hardworkar <arsmoroz@ya.ru>
2021-08-19 12:55:14 +0700

    yep we're writing assembly

--- whatsinteresting/interest.py
+++ whatsinteresting/interest.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import numpy as np
+
 n = 24
 
 # #(arguments) <= InsBlockSize - 3
@@ -35,8 +37,43 @@ def f(x, y):
 def Q(i, j):
   return f(RightBr[i][j], LeftBr[i][j]) / sum(f(x, y) for (x, y) in zip(RightBr[i], LeftBr[i]))
 
+def getDecision(i):
+  return np.argmax([Q(i, j) for j in range(0, n)])
+
+tab_ins = [
+  # instructions operating on internal state only
+  ["Jmpl", 6], ["Jmpeq", 6],
+  ["Add", 6], ["Sub", 6], ["Mul", 6], ["Div", 6],
+  ["Mov", 4], ["Init", 4]
+]
+
+class Ins():
+  def ins_Jmpl(self, params, clean_params):
+    # yep...
+  def exec_ins(self, ins_idx, params):
+    ins_name = 'ins_' + tab_ins[ins_idx][0]
+    assert tab_ins[ins_idx][1] == len(params)
+    ins_method = getattr(self, ins_name)
+    clean_params = []
+    for i in range(0, len(params), 2):
+      assert i + 1 < len(params) 
+      clean_params.append((params[i] * n + params[i+1]) % m)
+    ins_method(params, clean_params)
+    
+    
+
+
+
+
 while True:
-  # select instruction head a[j] with probability Q(IP, j)
+  # select instruction head a[j] with max? probability Q(IP, j)
+  ins_idx = getDecision(InsPointer)
+
   # select arguments 
+  num_params = tab_ins[ins_idx][1]
+  for i in range(1, num_params + 1):
+    param_idx = getDecision(InsPointer + i)
+    params.append(param_idx)
   # execute the instruction
+  exec_ins(ins_idx, params)
 
